833b92d7a1ace6c5bef72a3add02cd47e6d33e67
syedhamza6448 <164435480+syedhamza6448@users.noreply.github.com>
2024-12-14 15:59:05 +0500

    Homepage Almost Complete
    Only have to work on footer.

--- Assets/js/home.js
+++ Assets/js/home.js
@@ -1,41 +1,41 @@
 fadeRight = document.querySelector('.fadeRight');
 
 window.addEventListener('scroll', () => {
-    const scrollValue = window.scrollY;
-    const fadeRate = 1 - scrollValue / 200; 
-    fadeRight.style.opacity = fadeRate < 0 ? 0 : fadeRate;
-    fadeRight.style.transform = `translateX(${scrollValue}px)`;
-  });
+  const scrollValue = window.scrollY;
+  const fadeRate = 1 - scrollValue / 200;
+  fadeRight.style.opacity = fadeRate < 0 ? 0 : fadeRate;
+  fadeRight.style.transform = `translateX(${scrollValue}px)`;
+});
 
-  fadeLeft = document.querySelector('.fadeLeft');
+fadeLeft = document.querySelector('.fadeLeft');
 
-  window.addEventListener('scroll', () => {
-    const scrollValue = window.scrollY;
-    const fadeRate = 1 - scrollValue / 200;
-    const moveRate = scrollValue / 2;
-    
-    fadeLeft.style.opacity = fadeRate < 0 ? 0 : fadeRate;
-    fadeLeft.style.transform = `translateX(-${moveRate}px)`; 
-  });
+window.addEventListener('scroll', () => {
+  const scrollValue = window.scrollY;
+  const fadeRate = 1 - scrollValue / 200;
+  const moveRate = scrollValue / 2;
 
-  fadeUp = document.querySelector('.fadeUp');
+  fadeLeft.style.opacity = fadeRate < 0 ? 0 : fadeRate;
+  fadeLeft.style.transform = `translateX(-${moveRate}px)`;
+});
 
-  window.addEventListener('scroll', () => {
-    const scrollValue = window.scrollY;
-    const fadeRate = 1 - scrollValue / 200;
-    const moveRate = scrollValue / 2;
-    
-    fadeUp.style.opacity = fadeRate < 0 ? 0 : fadeRate;
-    fadeUp.style.transform = `translateY(-${moveRate}px)`; 
-  });
+fadeUp = document.querySelector('.fadeUp');
 
-  fadeDown = document.querySelector('.fadeDown');
+window.addEventListener('scroll', () => {
+  const scrollValue = window.scrollY;
+  const fadeRate = 1 - scrollValue / 200;
+  const moveRate = scrollValue / 2;
 
-  window.addEventListener('scroll', () => {
-    const scrollValue = window.scrollY;
-    const fadeRate = 1 - scrollValue / 200;
-    const moveRate = scrollValue / 2;
-    
-    fadeDown.style.opacity = fadeRate < 0 ? 0 : fadeRate;
-    fadeDown.style.transform = `translateY(${moveRate}px)`; 
-  });
+  fadeUp.style.opacity = fadeRate < 0 ? 0 : fadeRate;
+  fadeUp.style.transform = `translateY(-${moveRate}px)`;
+});
+
+fadeDown = document.querySelector('.fadeDown');
+
+window.addEventListener('scroll', () => {
+  const scrollValue = window.scrollY;
+  const fadeRate = 1 - scrollValue / 200;
+  const moveRate = scrollValue / 2;
+
+  fadeDown.style.opacity = fadeRate < 0 ? 0 : fadeRate;
+  fadeDown.style.transform = `translateY(${moveRate}px)`;
+});
